64e30d942f49c9ad82765c1f905b532ab492912b
Carrie Ryan <cryan1205@gmail.com>
2017-05-19 20:29:49 -0400

    first commit

---
+++ Week13JavaScriptPractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week13JavaScriptPractice.MvcApplication" Language="C#" %>
---
+++ Week13JavaScriptPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week13JavaScriptPractice.Startup))]
+namespace Week13JavaScriptPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
